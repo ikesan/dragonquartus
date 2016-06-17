1606ee913c5b261cf8b6e4708e2f9a1b535462cc
MURATA SATOSHI <a0146027@ws23.(none)>
2016-06-17 18:52:17 +0900

    ALU

--- output_files/ALUMethods.v
+++ output_files/ALUMethods.v
@@ -1,3 +1,4 @@
+//#
 module adder (in1,in2,out,S,V,Z,C);
 	input signed  [15:0] in1,in2;
 	output signed [15:0] out;
@@ -5,16 +6,15 @@ module adder (in1,in2,out,S,V,Z,C);
 	
 	wire signed [16:0] ans;	
 	assign ans = in1 + in2;
-	
 	assign out = ans [15:0];
 	assign S = ans[15];
-	assign Z = isZero(ans);
+	assign Z = !(|ans);
 	assign C = ans[16];
 	assign V = C;
 	
 endmodule
 	
-	
+//#
 module subber (in1,in2,out,S,V,Z,C);
 	input signed  [15:0] in1,in2;
 	output signed [15:0] out;
@@ -25,13 +25,13 @@ module subber (in1,in2,out,S,V,Z,C);
 	
 	assign out = ans [15:0];
 	assign S = ans[15];
-	assign Z = isZero(ans);
+	assign Z = !(|ans);
 	assign C = ans[16];
 	assign V = C;
 	
 endmodule
 	
-	
+//#	
 module ALUAnder (in1,in2,out,S,V,Z,C);
 	input  [15:0] in1,in2;
 	output  [15:0] out;
@@ -42,12 +42,13 @@ module ALUAnder (in1,in2,out,S,V,Z,C);
 	
 	assign out = ans [15:0];
 	assign S = ans[15];
-	assign Z = isZero(ans);
+	assign Z = !(|ans);
 	assign C = 0;
 	assign V = 0;
 	
 endmodule
-	
+
+//#	
 module ALUOrer (in1,in2,out,S,V,Z,C);
 	input  [15:0] in1,in2;
 	output  [15:0] out;
@@ -58,13 +59,92 @@ module ALUOrer (in1,in2,out,S,V,Z,C);
 	
 	assign out = ans [15:0];
 	assign S = ans[15];
-	assign Z = isZero(ans);
+	assign Z = !(|ans);
 	assign C = 0;
 	assign V = 0;
 	
 endmodule
 	
-module ALUSLA (in1,in2,out,S,V,Z,C);
-
-end module
+//#
+module ALUXOrer (in1,in2,out,S,V,Z,C);
+	input  [15:0] in1,in2;
+	output  [15:0] out;
+	output S,Z,C,V;
 	
+	wire [16:0] ans;	
+	assign ans = in1 ^ in2;
+	
+	assign out = ans [15:0];
+	assign S = ans[15];
+	assign Z = !(|ans);
+	assign C = 0;
+	assign V = 0;
+	
+endmodule
+
+module ALUSLL (in,d,out,S,V,Z,C);
+//fill 0 :: not yet C
+	input [15:0] in;
+	input [3:0] d;
+	output  [15:0] out;
+	output S,Z,C,V;
+	wire [15:0] ans;	
+	assign ans = in << d;
+	
+	assign out = ans [15:0];
+	assign S = ans[15];
+	assign Z = !(|ans);
+	assign C = 0;
+	assign V = 0; //always 0
+	
+	
+endmodule
+
+module ALUSLR (in,d,out,S,V,Z,C);
+//rotate
+	input [15:0] in;
+	input [3:0] d;
+	output  [15:0] out;
+	output S,Z,C,V;
+	wire [15:0] ans;	
+	assign ans = in <<< d;
+	
+	assign out = ans [15:0];
+	assign S = ans[15];
+	assign Z = !(|ans);
+	assign C = 0;
+	assign V = 0; //always 0
+endmodule
+
+//#
+module ALUSRL (in,d,out,S,V,Z,C);
+//rotate :: OK 
+	input [15:0] in;
+	input [3:0] d;
+	output  [15:0] out;
+	output S,Z,C,V;
+	wire [15:0] ans;	
+	assign ans = in >> d;
+	
+	assign out = ans [15:0];
+	assign S = ans[15];
+	assign Z = !(|ans);
+	assign C = 0;
+	assign V = 0; //always 0
+endmodule
+
+module ALUSRA (in,d,out,S,V,Z,C);
+//arithmetic not yet C
+	input signed [15:0] in;
+	input [3:0] d;
+	output signed   [15:0] out;
+	output S,Z,C,V;
+	wire [15:0] ans;	
+	assign ans = in >>> d;
+	
+	assign out = ans [15:0];
+	assign S = ans[15];
+	assign Z = !(|ans);
+	assign C = 0;
+	assign V = 0; //always 0
+endmodule
--- output_files/isZero.v
+++ output_files/isZero.v
@@ -4,9 +4,8 @@ module isZero (in,out);
 	
 	wire [16:0] ans;	
 	assign ans = in;
-	assign out = ans[15] | ans[14] | ans[13] | ans[12] 
+	assign out =!( ans[15] | ans[14] | ans[13] | ans[12] 
 				| ans[11] | ans[10] | ans[9] | ans[8] 
 				| ans[7] | ans[6] | ans[5] | ans[4] 
-				| ans[3] | ans[2] | ans[1] | ans[0];   	
+				| ans[3] | ans[2] | ans[1] | ans[0]);   	
 endmodule
-	
