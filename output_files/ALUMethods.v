cdb32bb8b458ff2dc837d24e5abd0abdfec4cc16
= <=>
2016-06-23 16:27:02 +0900

    test

--- output_files/ALUMethods.v
+++ output_files/ALUMethods.v
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //# out[S,V,Z,C,15:0] -> [19:0]
 module ALUinSVZC (in,S,V,Z,C,out);
   input [15:0] in;
@@ -228,5 +228,3 @@ module ALUSRA (in,d,out,S,V,Z,C);
 	assign C = ans[0];
 	assign V = 0; //always 0
 endmodule
-=======
->>>>>>> 3b3b289087bfe94056a30c4448e258c30e5a1dc9
