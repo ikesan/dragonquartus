a3a97374130afa627b952f2111d6e0e93969cabb
ikesan <hogehogefugagugugugugugug@gmail.com>
2016-06-24 14:48:21 +0900

    bug increased pc

--- output_files/isZero.v
+++ output_files/isZero.v
@@ -1,11 +1,5 @@
 module isZero (in,out);
-	input signed [15:0] in;
+	input [15:0] in;
 	output out;
-	
-	wire [16:0] ans;	
-	assign ans = in;
-	assign out =!( ans[15] | ans[14] | ans[13] | ans[12] 
-				| ans[11] | ans[10] | ans[9] | ans[8] 
-				| ans[7] | ans[6] | ans[5] | ans[4] 
-				| ans[3] | ans[2] | ans[1] | ans[0]);   	
+	assign out = !( |in);   	
 endmodule
