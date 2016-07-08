f218ad9f7a8f6ed012334e8bc062cc7e11d2fe8c
murata <murata@kmc.gr.jp>
2016-07-08 09:21:38 +0900

    .pyをきれいに

--- fromhex.py
+++ fromhex.py
@@ -1,70 +1,36 @@
 import sys,re
-if __name__ != "__main__" : exit(0)
-    
-def tobin16(line):
-    bindic = {
-        "0":"0000", "1":"0001", "2":"0010", "3":"0011" ,
-        "4":"0100", "5":"0101", "6":"0110", "7":"0111" ,
-        "8":"1000", "9":"1001", "a":"1010", "b":"1011" ,
-        "c":"1100", "d":"1101", "e":"1110", "f":"1111" ,
-    }
-    return bindic[line[0]] + bindic[line[1]] + bindic[line[2]] + bindic[line[3]]
+from tohex import * 
 
-bin3toNum = {
-    "000":"0","001":"1","010":"2","011":"3",
-    "100":"4","101":"5","110":"6","111":"7"
-}
-
-bin4toNum = {
-    "0000":"0","0001":"1","0010":"2","0011":"3",
-    "0100":"4","0101":"5","0110":"6","0111":"7",
-    "1000":"8","1001":"9","1010":"a","1011":"b",
-    "1100":"c","1101":"d","1110":"e","1111":"f",    
-}
-
-def bin8toNum (d):
-    return bin4toNum[d[0:4]] + bin4toNum[d[4:8]]
-def reg(r):
-    return "r" + bin3toNum[r]
+def tobin16(line): return "".join([num_bin4[line[i]] for i in range(4)])
+def bin8_num2(d): return bin4_num[d[0:4]] + bin4_num[d[4:8]]
+def reg(r): return "r" + bin3_num[r]
 
 def getCmd(line):
     line = [m.lower() for m in line]
     line = tobin16(line)
     if line[0:2] == "11":
-        rs = line[2:5]
-        rd = line[5:8]
-        op3 = line[8:12]
-        d = line[12:16]
-        cmddic = {"0000":"add","0001":"sub","0010":"and","0011":"or","0100":"xor","0101":"cmp","0110":"mov"}
-        if op3 in cmddic :
-            return cmddic[op3] + " " +reg(rd) + "," + reg(rs)
-        cmddic = { "1000":"sll","1001":"slr","1010":"srl","1011":"sra",}
-        if op3 in cmddic :
-            return cmddic[op3] + " " +reg(rs) + "," + d
+        rs,rd,op3,d = line[2:5],line[5:8],line[8:12],line[12:16]
+        if op3 in bin_cmd["alu"] :
+            return bin_cmd["alu"][op3] + " " +reg(rd) + "," + reg(rs)
+        if op3 in bin_cmd["shift"] :
+            return bin_cmd["shift"][op3] + " " +reg(rs) + "," + bin4_num[d]
         if op3 == "1111": return "hlt"
         if op3 == "1100": return "in " + reg(rd)
         if op3 == "1101": return "out " + reg(rs)
     if line[0] == "0" :
-        ra = line[2:5]
-        rb = line[5:8]
-        d = line[8:16]
-        op2 = {"00":"ld","01":"st"}[line[0:2]]
-        return op2 + " "+ reg(ra) + "," + bin8toNum(d)  + "(" + reg(rb) +  ")"
+        op2,ra,rb,d = line[0:2],line[2:5],line[5:8],line[8:16]
+        return bin_cmd["addr"][op2]+" "+reg(ra)+","+bin8_num2(d)+"("+reg(rb)+")"
     if line[0:2] == "10":
-        op2 = line[2:5]
-        rb = line[5:8]
-        d  = line[8:16]
-        cmddic = {"000":"li","001":"addi","010":"subi"}
-        if op2 in cmddic:
-            return cmddic[op2] + " " + reg(rb) +  "," + bin8toNum(d)
-        if op2 == "100":
-            return "b " + bin8toNum(d)
+        op2,rb,d = line[2:5],line[5:8],line[8:16]
+        if op2 in bin_cmd["imm"]:
+            return bin_cmd["imm"][op2] + " " + reg(rb) +  "," + bin8_num2(d)
+        if op2 == "100": 
+            return "b " + bin8_num2(d)
         if op2 == "111":
-            cmddic = {"000":"be","001":"blt","010":"ble","011":"bne"}
             cond = line[5:8]
-            return cmddic[cond] + " " + bin8toNum(d)
+            return bin_cmd["b"][cond] + " " + bin8_num2(d)
  
-        
-while True:
-    line = sys.stdin.readline()
-    print(getCmd(line))
+if __name__ == "__main__" :         
+    while True:
+        line = sys.stdin.readline()
+        print(getCmd(line))
--- tohex.py
+++ tohex.py
@@ -1,92 +1,77 @@
-#grammer
-#reg := r0 .. r7
-#[add/sub/and/or/xor/cmp/mov] reg,reg
-#[sll/slr/srl/sra] reg, A (16進数1桁)
-#[li/addi/subi] reg,AA (16進数2桁)
-#[b/be/blt/ble/bne] AA (16進数2桁)
-#[in/out] reg
-#hlt
-#[ld/st] reg,AA(reg) (16進数2桁)
-
+# 使用例
+#add r0,r1 # c800
+#sub r0 r1 # c810 (句読点は空白でも可)
+#add 0 1   # c800 (レジスタを示す r は省略可能)
+#sll r7 f  # c08f (レジスタはr0..r7まで)
+#li r0 fe  # 80fe
+#b ff      # a0ff
+#in r3     # c3c0
+#ld r2 AA(r1) # 11aa
 
 import sys,re
-if __name__ != "__main__" : exit(0)
-    
 
-def bin2hex(bin16):
-    bindic = {
-        "0000": "0","0001": "1","0010": "2","0011": "3",
-        "0100": "4","0101": "5","0110": "6","0111": "7",
-        "1000": "8","1001": "9","1010": "a","1011": "b",
-        "1100": "c","1101": "d","1110": "e","1111": "f",
-    }
-    #0000111100001111 -> 0f0f
-    res  = bindic[bin16[0:4]]
-    res += bindic[bin16[4:8]]
-    res += bindic[bin16[8:12]]
-    res += bindic[bin16[12:16]]
-    return res
+def reverseDic(dic) : 
+    return { v:k for k,v in dic.items() }
 
-num2bin3 = {
+num_bin3 = {
     "0":"000","1":"001","2":"010","3":"011",
-    "4":"100","5":"101","6":"110","7":"111"
+    "4":"100","5":"101","6":"110","7":"111",
 }
-num2bin4 = {
+bin3_num = reverseDic(num_bin3)
+num_bin4 = {
     "0":"0000","1":"0001","2":"0010","3":"0011",
     "4":"0100","5":"0101","6":"0110","7":"0111",
     "8":"1000","9":"1001","a":"1010","b":"1011",
     "c":"1100","d":"1101","e":"1110","f":"1111",    
 }
-def num2bin8(d):
-    if len(d) == 1:
-        return "0000" + num2bin4[d]
-    return num2bin4[d[0]] + num2bin4[d[1]]
+bin4_num = reverseDic(num_bin4)
+cmd_bin = {
+    "alu" : { "add":"0000","sub":"0001","and":"0010","or":"0011","xor":"0100","cmp":"0101","mov":"0110" },
+    "shift" : { "sll":"1000","slr":"1001","srl":"1010","sra":"1011" },
+    "imm" : { "li":"000","addi":"001","subi":"010" },
+    "addr" : { "ld":"00","st":"01" },
+    "b": { "be":"000","blt":"001","ble":"010","bne":"011" }
+}
+bin_cmd = { k:reverseDic(v) for k,v in cmd_bin.items()}
+
+#0000111100001111 => 0f0f
+def bin2hex(bin16):
+    return "".join([ bin4_num[bin16[4 * i:4 * i + 4]] for i in range(4)])
+
+#ff => 11111111,f => 00001111
+def num2_bin8(d):
+    if len(d) == 1 : return "0000" + num_bin4[d]
+    else : return num_bin4[d[0]] + num_bin4[d[1]]
 
 def getBin(line):
-    match = re.findall(r'(add|sub|and|or|xor|cmp|mov)[ ,]r?([0-7])[ ,]r?([0-7])',line,re.I )     
-    if match :        
-        match = [m.lower() for m in match[0]]
-        cmddic = {"add":"0000","sub":"0001","and":"0010","or":"0011","xor":"0100","cmp":"0101","mov":"0110"}
-        return bin2hex("11" + num2bin3[match[2]] + num2bin3[match[1]]+cmddic[match[0]] + "0000")
-
-    match = re.findall(r'(sll|slr|srl|sra)[ ,]r?([0-7])[ ,]([0-9a-f])',line,re.I)
-    if match :        
-        match = [m.lower() for m in match[0]]
-        cmddic = { "sll":"1000","slr":"1001","srl":"1010","sra":"1011",}
-        return bin2hex("11" + "000" + num2bin3[match[1]]+cmddic[match[0]] + num2bin4[match[2]])
-
-    match = re.findall(r'(li|addi|subi)[ ,]r?([0-7])[ ,]([0-9a-f]{1,2})',line,re.I)
-    if match :        
-        match = [m.lower() for m in match[0]]
-        cmddic = { "li":"000","addi":"001","subi":"010"}
-        return bin2hex("10" + cmddic[match[0]] + num2bin3[match[1]] + num2bin8(match[2]))
-
-    match = re.findall(r'(b|be|blt|ble|bne)[ ,]([0-9a-f]{1,2})',line,re.I)
-    if match :        
-        match = [m.lower() for m in match[0]]
-        if match[0] == "b":
-            return bin2hex("10100000" + num2bin8(match[1]))
-        cmddic = {"be":"000","blt":"001","ble":"010","bne":"011"}
-        return bin2hex("10111"+cmddic[match[0]] + num2bin8(match[1]))
-
-    match = re.findall(r'(in|out)[ ,]r?([0-7])',line,re.I)
-    if match :        
-        match = [m.lower() for m in match[0]]
-        if match[0] == "in":
-            return bin2hex("11"+"000"+num2bin3[match[1]] + "1100"+"0000")
-        if match[0] == "out":
-            return bin2hex("11"+num2bin3[match[1]]+"000" + "1101"+"0000")
+    def getMatched(regex):
+        match = re.findall(regex,line,re.I )     
+        if match : return [m.lower() for m in match[0]]
+        else : return []  
+    reg = num_bin3
+    d1  = num_bin4  
+    d2  = num2_bin8    
+    m = getMatched(r'(add|sub|and|or|xor|cmp|mov)[ ,]r?([0-7])[ ,]r?([0-7])')
+    if m : return bin2hex("11" + reg[m[2]] + reg[m[1]]+ cmd_bin["alu"][m[0]] + "0000")
+    m = getMatched(r'(sll|slr|srl|sra)[ ,]r?([0-7])[ ,]([0-9a-f])')
+    if m : return bin2hex("11" + "000" + reg[m[1]]+cmd_bin["shift"][m[0]] + d1[m[2]])
+    m = getMatched(r'(li|addi|subi)[ ,]r?([0-7])[ ,]([0-9a-f]{1,2})')
+    if m : return bin2hex("10" + cmd_bin["imm"][m[0]] + reg[m[1]] + d2(m[2]))
+    m = getMatched(r'(ld|st)[ ,]r?([0-7])[ ,]([0-9a-f]{2})\(r?([0-7])\)')
+    if m : return bin2hex(cmd_bin["addr"][m[0]] + reg[m[1]] + reg[m[3]] + d2(m[2]))
+    m = getMatched(r'(b|be|blt|ble|bne)[ ,]([0-9a-f]{1,2})')
+    if m : 
+        if m[0] == "b": return bin2hex("10100000" + d2(m[1]))
+        return bin2hex("10111"+cmd_bin["b"][m[0]] + d2(m[1]))
+    m = getMatched(r'(in|out)[ ,]r?([0-7])')
+    if m :        
+        if m[0] == "in": return bin2hex("11"+"000"+reg[m[1]] + "1100"+"0000")
+        if m[0] =="out": return bin2hex("11"+reg[m[1]]+"000" + "1101"+"0000")
         assert(0)
+    if re.findall(r'(hlt)',line,re.I) : return bin2hex("1111""1111""1111""1111")
+    assert(0)
 
-    match = re.findall(r'(hlt)',line,re.I)
-    if match : return bin2hex("1111""1111""1111""1111")
-
-    match = re.findall(r'(ld|st)[ ,]r?([0-7])[ ,]([0-9a-f]{2})\(r?([0-7])\)',line,re.I)
-    if match :        
-        match = [m.lower() for m in match[0]]
-        cmddic = { "ld":"00","st":"01"}
-        return bin2hex(cmddic[match[0]] + num2bin3[match[1]] + num2bin3[match[3]] + num2bin8(match[2]))
-    assert(False)
-while True:
-    line = sys.stdin.readline()
-    print(getBin(line))
+if __name__ == "__main__" :
+    while True:
+        line = sys.stdin.readline()
+        print(getBin(line))
